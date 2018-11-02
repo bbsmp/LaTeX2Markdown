69f332b6f17de08a86b8e9257204c27fdcd9e19e
bbsmp <qinmetec@163.com>
2018-11-02 18:15:25 +0800

    enhance

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         name='latex2markdown-bbsmp',
         author="PengMo",
         author_email="qinmetec@163.com",
-        version='0.0.3.3',
+        version='0.0.3.4',
         py_modules=['latex2markdown-bbsmp'],
         include_package_data=True,
         packages=find_packages("."),
